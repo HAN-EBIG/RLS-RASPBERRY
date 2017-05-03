171c830fde9c6e3fad61e1fcd12bf09903b913b1
Uwe van Heesch <uwe@vanheesch.net>
2017-05-03 19:03:44 +0200

    Added passwords for influx using properties file encrypted by vault.

--- installController.sh
+++ installController.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ansible-playbook playbook.yml -i hosts --ask-pass --become -c paramiko
+ansible-playbook playbook.yml -i hosts --vault-password-file vault_pass.txt --ask-pass --become -c paramiko
