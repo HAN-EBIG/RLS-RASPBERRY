327d3070ee6a7658efadac6900d8295e4fbde618
Uwe van Heesch <uwe@vanheesch.net>
2017-05-03 12:43:04 +0200

    Change pi standard password in ansible script. Provide example for wpa_supplicant.conf for Eduroam Corrected bug in systemstats script

--- sendSystemStats.sh
+++ sendSystemStats.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-EBIG_IP=`ip route get 1 | awk '{print $NF;exit}'`
-DISK_AVAIL=`df -hm --output=avail / | tail -n +2`
-MEM_AVAIL=`free -m | awk '{print $4}'| head -2| tail -1`
+EBIG_IP=`ip route get 1 | awk '{print $NF;exit}' | xargs`
+DISK_AVAIL=`df -hm --output=avail / | tail -n +2 | xargs`
+MEM_AVAIL=`free -m | awk '{print $4}'| head -2| tail -1 | xargs`
 CONTROLLER_STATE=`systemctl is-active ebig-controller.service >/dev/null 2>&1 && echo "1" || echo "0"`
 
 data_string="'controller-monitoring,host=${EBIG_IP} diskspace=${DISK_AVAIL},mem=${MEM_AVAIL},controllerRunning=${CONTROLLER_STATE}'"
